1eb4d8e61eec48202dec049328d2ddaa584ae3a4
Arif Esat Yılmaz <arif_esat@outlook.com>
2025-07-09 14:36:44 +0300

    Completed model evaluation

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -1,3 +1,4 @@
+import mlflow.sklearn
 import numpy as np
 import pandas as pd
 import pickle
@@ -12,6 +13,7 @@ import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 import json
+from dotenv import load_dotenv
 
 logger = logging.getLogger('model_evaluation')
 logger.setLevel('DEBUG')
@@ -59,3 +61,115 @@ def load_vectorizer(vectorizer_path: str) -> TfidfVectorizer:
         logger.error('Error loading vectorizer from %s: %s', vectorizer_path, e)
         raise
 
+def load_params(params_path: str) -> dict:
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters loaded from %s:',params_path)
+        return params
+    except Exception as e:
+        logger.error('Error occurred while loading parameters from %s: %s', params_path, e)
+        raise
+
+def evaluate_model(model, X_test: np.ndarray, y_test: np.ndarray):
+    try:
+        y_pred = model.predict(X_test)
+        report = classification_report(y_test, y_pred, output_dict=True)
+        cm = confusion_matrix(y_test, y_pred)
+
+        logger.debug('Model evaluation completed')
+
+    except Exception as e:
+        logger.error('Unexpected error occurred while evaluating model: %s', e)
+        raise
+
+def log_confusion_matrix(cm, dataset_name):
+    plt.figure(figsize=(8,6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    plt.title(f"Confusion Matrix for {dataset_name}")
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+
+    cm_file_path = f"confusion_matrix_{dataset_name}.png"
+    plt.savefig(cm_file_path)
+    mlflow.log_artifact(cm_file_path)
+    plt.close()
+
+def save_model_info(run_id: str, model_path: str, file_path: str) -> None:
+    try:
+        model_info = {
+            'run_id': run_id,
+            'model_path': model_path
+        }
+
+        with open(file_path, 'w') as file:
+            json.dump(model_info, file, indent=4)
+        logger.debug('Model info saved to %s', file_path)
+        
+    except Exception as e:
+        logger.error('Unexpected error occurred while saving model info: %s', e)
+        raise
+
+def main():
+    load_dotenv()
+    print(os.getenv('AWS-MLFLOW'))
+    # Get the AWS MLflow URI from .env file
+    aws_mlflow_uri = os.getenv('AWS-MLFLOW')
+
+    mlflow.set_tracking_uri(aws_mlflow_uri)
+
+    mlflow.set_experiment('dvc-pipeline-runs')
+
+    with mlflow.start_run() as run:
+        try:
+            root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+            params = load_params(os.path.join(root_dir, 'params.yaml'))
+
+            for key, value in params.items():
+                mlflow.log_param(key,value)
+
+            model = load_model(os.path.join(root_dir, 'lgbm_model.pkl'))
+            vectorizer = load_vectorizer(os.path.join(root_dir, 'tfidf_vectorizer.pkl'))
+
+            test_data = load_data(os.path.join(root_dir, 'data/interim/test_processed.csv'))
+
+            X_test_tfidf = vectorizer.transform(test_data['clean_comment'].values)
+            y_test = test_data['category'].values
+
+            input_example = pd.DataFrame(X_test_tfidf.toarray()[:5], columns=vectorizer.get_feature_names_out())
+
+            signature = infer_signature(input_example, model.predict(X_test_tfidf[:5]))
+
+            mlflow.sklearn.log_model(
+                model,
+                'lgmb_model',
+                signature=signature,
+                input_example=input_example
+            )
+
+            model_path = 'lbgm_model'
+            save_model_info(run.info.run_id, model_path, 'experiment_info.json')
+
+            mlflow.log_artifact(os.path.join(root_dir, 'tfidf_vectorizer.pkl'))
+
+            report, cm = evaluate_model(model, X_test_tfidf, y_test)
+
+            for label, metrics in report.items():
+                if isinstance(metrics, dict):
+                    mlflow.log_metric({
+                        f"test_{label}_precision": metrics['precision'],
+                        f"test_{label}_recall": metrics['recall'],
+                        f"test_{label}_f1-score": metrics['f1-score'],
+                    })
+
+            log_confusion_matrix(cm, "Test Data")
+
+            mlflow.set_tag("model_type", "LightGBM")
+            mlflow.set_tag("task", "Sentiment Analysis")
+            mlflow.set_tag("dataset", "Youtube Comments")
+            
+        except Exception as e:
+            logger.error(f"Failed to complete model evaluation: {e}")
+
+if __name__ == '__main__':
+    main()
