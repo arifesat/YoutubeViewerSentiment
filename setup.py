10fdec5caf30da75923f1114b4cbd4431850fbae
Arif Esat Yılmaz <arif_esat@outlook.com>
2025-07-10 17:51:34 +0300

    Configured app.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'youtube',
-    version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'entbappy73@gmail.com',
+    version= '1.0.0',
+    author= 'Arif Esat Yilmaz',
+    author_email= 'arif_esat@outlook.com',
     packages= find_packages(),
     install_requires = []
 
